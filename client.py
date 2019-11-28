0134caab19a180c05e222ff9cb1a323cb20ffa6c
edw1n94 <39121172+edw1n94@users.noreply.github.com>
2019-11-28 18:27:20 +0900

    Update client.py

--- client.py
+++ client.py
@@ -10,16 +10,12 @@ port = 9400
 class get_latency(Resource):
     def post(self):
         content = request.get_json(silent=True)
-        response_list = ping(content.get('client_ip'), size=40, count=3)
+        response_list = ping(content.get('client_ip'), size=45, count=50)
         return {'average_latency':response_list.rtt_avg_ms}
-
-
-
 
 class get_collecter_ip(Resource):
     def get(self):
         return socket.gethostbyname(socket.gethostname())
-
 
 api.add_resource(get_latency,'/get_latency')
 api.add_resource(get_collecter_ip,'/get_collecter_ip')
